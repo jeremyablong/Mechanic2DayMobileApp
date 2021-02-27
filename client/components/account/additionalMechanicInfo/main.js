b9d7961946a8c64abe689998765b27174de76c59
jeremy <blongjeremy@icloud.com>
2021-02-26 20:49:09 -0500

    push public - continuation of mechanic profile settings/form

--- client/components/account/additionalMechanicInfo/main.js
+++ client/components/account/additionalMechanicInfo/main.js
@@ -3,6 +3,8 @@ import { View, Text, Image, ScrollView, Dimensions } from 'react-native';
 import { Header, Left, Body, Right, Title, Subtitle, Button, Form, Item, Input, Label, Picker, Icon } from 'native-base';
 import styles from './styles.js';
 import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
+import AwesomeButtonBlue from 'react-native-really-awesome-button/src/themes/blue';
+import RBSheet from "react-native-raw-bottom-sheet";
 
 
 const { height, width } = Dimensions.get("window");
@@ -14,10 +16,41 @@ constructor(props) {
 
     this.state = {
         yearsOfExperience: 0,
-        degree: ""
+        degree: "",
+        employerName: "",
+        lengthOfEmployment: 0,
+        jobTitle: "",
+        duties: "",
+        jobs: []
     }
 }
+    addEmployer = () => {
+        console.log("addEmployer");
+
+        const { jobTitle, employerName, duties, lengthOfEmployment } = this.state;
+
+        this.setState({
+            jobs: [...this.state.jobs, {
+                job_title: jobTitle,
+                employer_name: employerName,
+                duties_responsibilities: duties,
+                length_of_employment_in_years: lengthOfEmployment
+            }]
+        }, () => {
+            this.RBSheet.close();
+        })
+    }
+    calculateReadinessOne = () => {
+        const { jobTitle, employerName, duties, lengthOfEmployment } = this.state;
+
+        if ((typeof jobTitle !== "undefined" && jobTitle.length > 0) && (typeof employerName !== "undefined" && employerName.length > 0) && (typeof duties !== "undefined" && duties.length > 0) && (lengthOfEmployment !== 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     render() {
+        const { jobs } = this.state;
         return (
             <Fragment>
                 <Header>
@@ -47,32 +80,112 @@ constructor(props) {
                                 }} />
                             </Item>
                             <Text style={{ marginTop: 15 }}>What type of degree in the auto industry do you have?</Text>
-                            <Item floatingLabel>
-                                <Label>What degree do you have?</Label>
-                                <Picker
-                                    mode="dropdown"
-                                    iosHeader="Select your education"
-                                    iosIcon={<Icon name="arrow-down" />}
-                                    style={{ width: width * 0.80 }}
-                                    selectedValue={this.state.degree}
-                                    defaultValue={this.state.degree}
-                                    onValueChange={(value) => {
-                                        this.setState({
-                                            degree: value
-                                        })
-                                    }}
-                                >
-                                <Picker.Item label="No-Degree" value="no-degree" />
-                                <Picker.Item label="Self-Taught" value="self-taught" />
-                                <Picker.Item label="Technical-Trade School" value="technical-trade-school" />
-                                <Picker.Item label="Associates" value="associates" />
-                                <Picker.Item label="Bachelors" value="bachelors" />
+                            <View style={{ marginTop: 15, marginBottom: 15 }}>
+                                <Picker 
+                                        placeholder={"Select your degree type..."}
+                                        placeholderTextColor={"grey"}
+                                        mode="dropdown"
+                                        iosHeader="Select your education"
+                                        iosIcon={<Icon name="arrow-down" />}
+                                        style={{ width: width * 0.80 }}
+                                        selectedValue={this.state.degree}
+                                        defaultValue={this.state.degree}
+                                        onValueChange={(value) => {
+                                            this.setState({
+                                                degree: value
+                                            })
+                                        }}
+                                    >
+                                    <Picker.Item label="No-Degree" value="no-degree" />
+                                    <Picker.Item label="Self-Taught" value="self-taught" />
+                                    <Picker.Item label="Technical-Trade School" value="technical-trade-school" />
+                                    <Picker.Item label="Associates" value="associates" />
+                                    <Picker.Item label="Bachelors" value="bachelors" />
                                 </Picker>
-                            </Item>
+                            </View>
                             
                         </Form>
+                        <AwesomeButtonBlue type={"secondary"} stretch={true} onPress={() => {
+                            this.RBSheet.open();
+                        }}>Add employment history</AwesomeButtonBlue>
+                        <View style={styles.marginBothWays}>
+                                {typeof jobs !== "undefined" && jobs.length > 0 ? jobs.map((job, index) => {
+                                    return (
+                                        <Fragment>
+                                            <View style={styles.boxed}>
+                                                <Text style={styles.myText}><Text style={styles.bold}>Title</Text>: {job.job_title}</Text>
+                                                <Text style={styles.myText}><Text style={styles.bold}>Co. Name</Text>: {job.employer_name}</Text>
+                                                <Text style={styles.myText}><Text style={styles.bold}>Employed for</Text> {job.length_of_employment_in_years} <Text style={styles.bold}>Years</Text></Text>
+                                                <Text style={styles.myText}><Text style={styles.bold}>Duties</Text>: {job.duties_responsibilities}</Text>
+                                            </View>
+                                            <View style={styles.hr} />
+                                        </Fragment>
+                                    );
+                                }) : null}
+                        </View>
+                        <View style={{ marginTop: 15 }}>
+                            <AwesomeButtonBlue type={"disabled"} stretch={true} onPress={() => {}}>Submit & Continue</AwesomeButtonBlue>
+                        </View>
                     </View>
                 </ScrollView>
+                <RBSheet
+                    ref={ref => {
+                        this.RBSheet = ref;
+                    }}
+                    height={height}
+                    openDuration={250}
+                    customStyles={{
+                        container: {
+                        justifyContent: "center",
+                        alignItems: "center"
+                        }
+                    }}
+                    >
+                    <View>
+                        <Text style={styles.header}>Please enter each employers imformation indvidually below...</Text>
+                        <Form>
+                            <Item style={styles.itemWidth} floatingLabel>
+                            <Label>Employer name</Label>
+                                <Input value={this.state.employerName} onChangeText={(value) => {
+                                    this.setState({
+                                        employerName: value
+                                    })
+                                }} />
+                            </Item>
+                            <Item style={styles.itemWidth} floatingLabel last>
+                                <Label>Length of employment (in years)</Label>
+                                <Input value={this.state.lengthOfEmployment} keyboardType={"number-pad"} onChangeText={(value) => {
+                                    this.setState({
+                                        lengthOfEmployment: Number(value)
+                                    })
+                                }} />
+                            </Item>
+                            <Item style={styles.itemWidth} floatingLabel last>
+                                <Label>Job Title</Label>
+                                <Input value={this.state.jobTitle} keyboardType={"number-pad"} onChangeText={(value) => {
+                                    this.setState({
+                                        jobTitle: value
+                                    })
+                                }} />
+                            </Item>
+                        </Form>
+                        <View style={styles.margin}>
+                            <AutoGrowingTextInput style={styles.itemWidth} value={this.state.duties} onChangeText={(value) => {
+                                this.setState({
+                                    duties: value
+                                })
+                            }} placeholderTextColor={"grey"} placeholder={'Enter your job duties and/or responsiblities...'} />
+                        </View>
+                        <View style={styles.margin}>
+                            {this.calculateReadinessOne() ? <AwesomeButtonBlue type={"secondary"} stretch={true} onPress={this.addEmployer}>Add employer</AwesomeButtonBlue> : <AwesomeButtonBlue type={"disabled"} stretch={true}>Add employer</AwesomeButtonBlue>}
+                            <View style={{ marginTop: 20 }}>
+                                <AwesomeButtonBlue type={"primary"} stretch={true} onPress={() => {
+                                    this.RBSheet.close();
+                                }}>Close Panel</AwesomeButtonBlue>
+                            </View>
+                        </View>
+                    </View>
+                </RBSheet>
             </Fragment>
         )
     }
--- client/components/account/additionalMechanicInfo/styles.js
+++ client/components/account/additionalMechanicInfo/styles.js
@@ -10,5 +10,37 @@ export default StyleSheet.create({
     },
     margin: {
         margin: 20
+    },
+    itemWidth: {
+        width: width * 0.80
+    },
+    header: {
+        fontSize: 18, 
+        fontWeight: "bold", 
+        textAlign: "center",
+        textDecorationLine: "underline",
+        margin: 10
+    },
+    marginBothWays: {
+        marginTop: 20,
+        marginBottom: 20
+    },
+    myText: {
+        fontSize: 18,
+        marginBottom: 8
+    },
+    boxed: {
+        width: "100%", 
+        minHeight: 150,
+        height: "100%"
+    },
+    bold: {
+        fontWeight: "bold"
+    },
+    hr: {
+        borderBottomColor: "black",
+        borderBottomWidth: 2,
+        marginTop: 15,
+        marginBottom: 15
     }
 })
